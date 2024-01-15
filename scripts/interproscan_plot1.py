dbcdeb289677152a42b51ce0bd63697e45be9b91
macvbookpro <mnevverkokcu@gmail.com>
2024-01-15 21:25:52 +0300

    last_update

--- scripts/interproscan_plot1.py
+++ scripts/interproscan_plot1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 #read csv
-df = pd.read_csv('/Users/macvbookpro/PycharmProjects/miuul/resource/ınterproscan/G_muris.tsv', sep='\t', names=list(range(0, 15)), engine='python', quoting=3)[[0, 3, 4, 5, 11, 12]]
+df = pd.read_csv('/resource/Interproscan/G_muris.tsv', sep='\t', names=list(range(0, 15)), engine='python', quoting=3)[[0, 3, 4, 5, 11, 12]]
 
 #get IPR annotation from column 11 for each gene
 df_ipr = df[[0, 11]]
